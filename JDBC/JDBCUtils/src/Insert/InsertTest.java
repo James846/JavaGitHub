7289e4553ec3717005edddd6e4009a3aaf3de26e
James846 <2236328881@qq.com>
2021-08-29 17:27:57 +0800

    上传

---
+++ JDBC/JDBCUtils/src/Insert/InsertTest.java
@@ -0,0 +1,26 @@
+package Insert;
+
+import JDBCTools.JDBCTools;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class InsertTest {
+    public static void main(String[] args)throws Exception{
+        testInsert();
+    }
+    public static void testInsert()throws Exception{
+        Long start=System.currentTimeMillis();
+        Connection conn= JDBCTools.getConnection();
+        String sql="insert into goods(name) values(?)";
+        PreparedStatement ps= conn.prepareStatement(sql);
+        for(int i=0;i<=200;i++){
+            ps.setObject(1,"name "+i);
+            ps.execute();
+        }
+        Long end=System.currentTimeMillis();
+        System.out.println("time: "+(end-start));
+        JDBCTools.CloseConnection(conn,ps);
+
+    }
+}
