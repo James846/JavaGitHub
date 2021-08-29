5fc3791232abb5c41ebd6c9cb3b22d7fb44b16ad
Ultimate illusion <76240908+James846@users.noreply.github.com>
2021-08-29 12:32:47 +0800

    Update Jdbc.java

--- JDBC/数据库测试/Jdbc.java
+++ JDBC/数据库测试/Jdbc.java
@@ -7,7 +7,7 @@ import java.sql.Statement;
 public class Jdbc {
     public static void main(String[] args)throws Exception{
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/regist","root","jimmy1016");
+        Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/regist","root","********");
         String sql="INSERT INTO register VALUES('123','123456')";
         Statement sta=conn.createStatement();
         boolean count=sta.execute(sql);
