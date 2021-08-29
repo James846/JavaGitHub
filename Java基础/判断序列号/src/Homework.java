e00006038b45f755836362503ce027453c31bdb5
James846 <2236328881@qq.com>
2021-08-29 15:02:39 +0800

    Lambda表达式

--- Java基础/判断序列号/src/Homework.java
+++
@@ -1,7 +0,0 @@
-public class Homework {
-    public static void main(String args[]){
-        Win win=new Win();
-        win.setTitle("输入序列号");
-        win.setBounds(10,10,460,360);
-    }
-}
--- Java基础/通讯录文本/src/Result.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class Result {
-    public static void main(String args[]){
-        WindowMenu win=
-                new WindowMenu("通讯录",500,500,800,200);
-        }
-}
-
