90a70e6d115eef652a097b89ca10907fefa46c3f
James846 <2236328881@qq.com>
2021-09-03 17:41:23 +0800

    数据库连接池

---
+++ JDBC/Druid 数据库连接池/src/Test.java
@@ -0,0 +1,26 @@
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import javax.sql.DataSource;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class Test {
+    public static void main(String[] args)throws Exception{
+        new Test().TestConnection();
+    }
+    public void TestConnection()throws Exception{
+        InputStream is=ClassLoader.getSystemClassLoader().getResourceAsStream("IP.txt");
+        Properties prop=new Properties();
+        prop.load(is);
+        String user=prop.getProperty("username");
+        String password=prop.getProperty("password");
+        String url=prop.getProperty("url");
+        String driverClass=prop.getProperty("driverClassName");
+        Class.forName(driverClass);
+        Connection conn= DriverManager.getConnection(url,user,password);
+        System.out.println(conn);
+        DataSource ds=DruidDataSourceFactory.createDataSource(prop);
+        System.out.println(ds.getConnection());
+    }
+}
